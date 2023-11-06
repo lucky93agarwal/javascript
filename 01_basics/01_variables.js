e71ba57dbaaadfd0884f5df488e00ef2019efaef
Lucky Agawral <53622073+lucky93agarwal@users.noreply.github.com>
2023-11-06 10:46:54 +0000

    undefined variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "lucky@gmail.com"
 var accountPassword = "123456"
 accountCity = "Lucknow"
+let accountState;
 
 accountEmail = "hdfc@gmail.com"
 accountPassword = "21212121"
@@ -11,4 +12,4 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
