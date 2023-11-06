0eb1cdf6c570301e1b2fc9fe9d0ee70e19007be0
Lucky Agawral <53622073+lucky93agarwal@users.noreply.github.com>
2023-11-06 11:03:55 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+let valueInNumberTwo = String(valueInNumber)
+// console.log(typeof valueInNumberTwo);
+// console.log(valueInNumber);
+// console.log(valueInNumberTwo);
+
+// "33" => 33
+// "33abc"=> NaN
+// true => 1; false => 0
+
+// ************************************ Operations ***************************************************
+let value = 3
+let negValue = -value
+console.log(negValue);
